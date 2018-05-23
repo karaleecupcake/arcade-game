ea58f3aa9a929aed9ffb8a5d0247fb10bedd2a5a
Karalee Shotola <karalee@utexas.edu>
2018-05-23 10:54:03 +0200

    Create new player and enemy objects

--- js/app.js
+++ js/app.js
@@ -58,8 +58,8 @@ Player.prototype.handleInput = function(key) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [new Enemy(-150, 50), new Enemy(-100, 145), new Enemy(-250, 225)];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
