c1b10dceb85dab7e8f7c7370a63e5952fad7592a
Karalee Shotola <karalee@utexas.edu>
2018-05-23 10:18:09 +0200

    Set image of player

--- js/app.js
+++ js/app.js
@@ -30,7 +30,9 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function() {
+  this.sprite = 'images/char-cat-girl.png';
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
